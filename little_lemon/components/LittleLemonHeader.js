185511ce8d472fd0f081ad9a0ad43eb894a6d969
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-11 14:45:58 +0530

    Added WelcomeScreen component

--- little_lemon/components/LittleLemonHeader.js
+++ little_lemon/components/LittleLemonHeader.js
@@ -4,8 +4,9 @@ import {View,Text} from 'react-native';
 export default function LittleLemonHeader(){
   return(
     <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
-      <Text style={{ padding: 40, fontSize: 30, color: 'black', textAlign: 'center' }} numberOfLines={2}>
-        Welcome to 
+      {/* numberOfLines={2} */}
+      <Text style={{ padding: 40, fontSize: 30, color: 'black', textAlign: 'center' }} >
+        {/* Welcome to  */}
         <Text style={{fontWeight:'bold'}}> Little Lemon </Text> {' '}
       </Text>
     </View>
