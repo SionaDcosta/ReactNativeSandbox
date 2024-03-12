b30c826f149bff526428010d60ae22f8da7aaa1a
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-12 07:11:58 +0530

    Added scrollable MenuItems component

--- little_lemon/App.js
+++ little_lemon/App.js
@@ -1,23 +1,27 @@
-import { View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
 import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
     <>
-    
-    <View style={{flex:1,backgroundColor:'#495E57'}}>
-      <LittleLemonHeader/>
-      <WelcomeScreen/>
-      {/* <MenuItems/> */}
+    <View style={styles.container}>
+      <LittleLemonHeader />
+      <MenuItems />
     </View>
-    
     <View>
       <LittleLemonFooter/>
     </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#495E57',
+  },
+});
--- little_lemon/components/LittleLemonHeader.js
+++ little_lemon/components/LittleLemonHeader.js
@@ -1,14 +1,42 @@
-import * as React from 'react';
-import {View,Text} from 'react-native';
 
-export default function LittleLemonHeader(){
-  return(
-    <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
-      {/* numberOfLines={2} */}
-      <Text style={{ padding: 40, fontSize: 30, color: 'black', textAlign: 'center' }} >
-        {/* Welcome to  */}
-        <Text style={{fontWeight:'bold'}}> Little Lemon </Text> {' '}
+
+// export default function LittleLemonHeader(){
+//   return(
+//     <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
+//       {/* numberOfLines={2} */}
+//       <Text style={{ padding: 40, fontSize: 30, color: 'black', textAlign: 'center' }} >
+//         {/* Welcome to  */}
+//         <Text style={{fontWeight:'bold'}}> Little Lemon </Text> {' '}
+//       </Text>
+//     </View>
+//   );
+// }
+
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function LittleLemonHeader() {
+  return (
+    <View style={headerStyles.container}>
+      <Text style={headerStyles.headerText}>
+	  Welcome to
+        <Text style={headerStyles.innerText}> Little Lemon</Text>
       </Text>
     </View>
   );
 }
+
+const headerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4CE14',
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: 'black',
+    textAlign: 'center',
+  },
+  innerText: {
+    fontWeight: 'bold',
+  },
+});
