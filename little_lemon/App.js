f607c0a7cd0f2ca0a70d055fbc907eaacd344db0
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-12 10:47:13 +0530

    Added MenuItems_SectionList component

--- little_lemon/App.js
+++ little_lemon/App.js
@@ -5,14 +5,15 @@ import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import MenuItems from './components/MenuItems';
 import MenuItems_FlatList from './components/MenuItems_FlatList';
-
+import MenuItems_SectionList from './components/MenuItems_SectionList';
 export default function App() {
   return (
     <>
     <View style={styles.container}>
       <LittleLemonHeader />
       {/* <MenuItems /> */}
-      <MenuItems_FlatList/>
+      {/* <MenuItems_FlatList/> */}
+      <MenuItems_SectionList/>
     </View>
     <View>
       <LittleLemonFooter/>
