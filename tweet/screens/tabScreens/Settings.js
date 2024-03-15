6d6bed7c6c73f4c691225484f6733d4e2321cd97
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-15 16:43:57 +0530

    Added 3 bottom tabs correctly with active and inactive tint color

--- tweet/screens/tabScreens/Settings.js
+++ tweet/screens/tabScreens/Settings.js
@@ -1,8 +1,14 @@
 import { signOut } from "firebase/auth";
 import { Button, Text, View } from "react-native";
 import { auth } from "../../firebaseConfig";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
+  
 export default function Settings() {
   return (
     <View>
