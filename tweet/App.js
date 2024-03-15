79955102c7563e8cae71e19938a8207dee76f7eb
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-15 15:47:55 +0530

    created beto page with tweets

---
+++ tweet/App.js
@@ -0,0 +1,16 @@
+import "react-native-gesture-handler";
+import Navigation from "./Navigation";
+
+
+export default function App() {
+  return <Navigation/>
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ tweet/screens/Feed.js
@@ -0,0 +1,12 @@
+ import {Text, View} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+ export default function Feed(){
+    return(
+        <SafeAreaView>
+            <Text>Feed</Text>
+        </SafeAreaView>
+            
+        
+    )
+ }
