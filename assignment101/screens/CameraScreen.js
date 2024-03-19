e1c535ceca4ca00930b9175484fbb167314055c1
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-19 10:33:20 +0530

    setting up camera

--- assignment101/screens/CameraScreen.js
+++ assignment101/screens/CameraScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-
+import { Link, Stack } from 'expo-router';
 const CameraScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Camera Screen</Text>
+      <Link href="/day11/camera" asChild>
+        <Button title="Go to Camera" />
+      </Link>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
