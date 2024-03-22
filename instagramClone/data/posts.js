24e20c0707aa32d1c76535db7da4bdf86eaf7e2e
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-22 12:10:08 +0530

    Done till importing images in PostHeader

---
+++ instagramClone/data/posts.js
@@ -0,0 +1,123 @@
+import { Users } from "./Users";
+
+export const Posts = [
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[0].user,
+        likes: 15,
+        caption : 'FirstPost',
+        profile_picture: Users[0].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[1].user,
+        likes: 15,
+        caption : 'SecondPost',
+        profile_picture: Users[1].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[2].user,
+        likes: 15,
+        caption : 'ThirdPost',
+        profile_picture: Users[2].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[3].user,
+        likes: 15,
+        caption : 'FourthPost',
+        profile_picture: Users[3].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[4].user,
+        likes: 15,
+        caption : 'FifthPost',
+        profile_picture: Users[4].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[5].user,
+        likes: 15,
+        caption : 'SixthPost',
+        profile_picture: Users[5].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    },
+    {
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1688497831081-836510928186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWxzfGVufDB8fDB8fHww',
+        user: Users[6].user,
+        likes: 15,
+        caption : 'SeventhPost',
+        profile_picture: Users[6].image,
+        comments : [
+            {
+                user: 'gwenfernandes',
+                comment: 'Wow'
+            },
+            {
+                user: 'glen',
+                comment: 'Wow'
+            }
+        ]
+    }
+];
--- instagramClone/screens/HomeScreen.js
+++ instagramClone/screens/HomeScreen.js
@@ -1,14 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../components/home/Header';
 import Stories from '../components/Stories';
-
+import Post from '../components/home/Post';
+import { Posts } from '../data/posts';
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container} >
       <Header/>
       <Stories/>
+      <ScrollView>
+        {Posts.map((post,index) => (
+          <Post post={post} key={index}/>
+        ))}
+      </ScrollView>
+      
     </SafeAreaView>
   )
 }
