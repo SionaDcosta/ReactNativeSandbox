f1ba19c93d204ad24a3baf95f08ba65eccb80560
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-25 11:18:11 +0530

    BottomTabs done

--- instagramClone/components/home/BottomTabs.js
+++ instagramClone/components/home/BottomTabs.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useState } from 'react'
 import { Divider } from 'react-native-elements'
@@ -36,26 +36,49 @@ const BottomTabs = ({icons}) => {
     const [activeTab, setActiveTab] = useState('Home')
     const Icon = ({icon}) => (
         <TouchableOpacity onPress={()=> setActiveTab(icon.name)}>
-            <Image source={icon.active} style={styles.icon}/>
-        </TouchableOpacity>
-    )
+            <Image source={activeTab === icon.name? icon.active: icon.inactive} 
+            style={[styles.icon, 
+                    icon.name === 'Profile'? styles.profilePic(): null,
+                    activeTab === 'Profile' && icon.name === activeTab? 
+                    styles.profilePic(activeTab)
+                    :null,]}/>
+        </TouchableOpacity>)
+    
   return (
-    <View style={styles.container}>
+    <View style={styles.wrapper}>
+        <Divider width={1} orientation='vertical'/>
+        <View style={styles.container}>
       {icons.map((icon,index)=>(
         <Icon key={index} icon={icon}/>
       ))}
+    </View>
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
+    wrapper:{
+        position:'absolute',
+        width: '100%',
+        bottom:'0%',//3%
+        zIndex: 999,
+        backgroundColor:'#000'
+    },
     container:{
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height:50,
+        paddingTop: 10
     },
     icon:{
         width:30,
         height: 30
-    }
+    },
+    profilePic:(activeTab = '')=>({
+        borderRadius:50,
+        borderWidth:activeTab === 'Profile'? 2: 0,
+        borderColor: '#fff'
+    })
 })
 export default BottomTabs;
