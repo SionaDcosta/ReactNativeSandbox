7c47d7ec9d5c596c9cb114f4ee21b348da9eea22
SionaDcosta <siona.dcosta@zapcg.com>
2024-03-28 12:54:42 +0530

    just commented firebase in app.js

--- instagramClone/App.js
+++ instagramClone/App.js
@@ -4,19 +4,19 @@ import NewPostScreen from "./screens/NewPostScreen";
 import SignedInStack from "./navigation";
 import 'react-native-gesture-handler';
 import React, {useState, useEffect} from "react";
-import {initializeApp} from '@firebase/app'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "@firebase/auth";
+// import {initializeApp} from '@firebase/app'
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "@firebase/auth";
  
-const firebaseConfig = {
-  apiKey: "AIzaSyDWUsHaMWen7ueGvxZRtqtDPsjyOXSVp8I",
-  authDomain: "rn-instgaramclone.firebaseapp.com",
-  projectId: "rn-instgaramclone",
-  storageBucket: "rn-instgaramclone.appspot.com",
-  messagingSenderId: "474479315321",
-  appId: "1:474479315321:web:ffe10bc3d6c188422b87a8"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDWUsHaMWen7ueGvxZRtqtDPsjyOXSVp8I",
+//   authDomain: "rn-instgaramclone.firebaseapp.com",
+//   projectId: "rn-instgaramclone",
+//   storageBucket: "rn-instgaramclone.appspot.com",
+//   messagingSenderId: "474479315321",
+//   appId: "1:474479315321:web:ffe10bc3d6c188422b87a8"
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 export default function App() {
   return <SignedInStack/>
 }
