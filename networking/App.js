17406915f8ebcf1d21938dda56182687bf2c6e51
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-01 16:31:21 +0530

    Loading State added

--- networking/App.js
+++ networking/App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { useState, useEffect } from 'react';
 
 export default function App() {
   const [ postList, setPostList ] = useState([])
+  const [isLoading, setIsLoading] = useState([true])
   const fetchData = async( limit = 10 ) => {
     const response = await fetch (
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     );
     const data = await response.json()
-    setPostList(data)
+    setPostList(data);
+    setIsLoading(false);
   };
 
   useEffect( ()=>{
     fetchData();
-  })
+  });
+
+  if( isLoading ){
+    return(
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff"/>
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.listContainer}>
@@ -79,5 +90,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     marginBottom: 12
+  },
+  loadingContainer:{
+    flex:1,
+    backgroundColor:"#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent:"center",
+    alignItems:"center"
   }
 });
