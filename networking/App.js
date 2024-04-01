75cd6c7394e353d94bd4a9461aecc597682e9332
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-01 17:06:45 +0530

    Pull to refresh and fetch 20 records after refresh DONE.

--- networking/App.js
+++ networking/App.js
@@ -5,19 +5,42 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
   const [ postList, setPostList ] = useState([])
-  const [isLoading, setIsLoading] = useState([true])
-  const fetchData = async( limit = 10 ) => {
-    const response = await fetch (
-      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-    );
-    const data = await response.json()
-    setPostList(data);
-    setIsLoading(false);
+  const [isLoading, setIsLoading] = useState(true)
+  const [refreshing, setRefreshing] = useState(false)
+
+  // const fetchData = async( limit = 10 ) => {
+  //   const response = await fetch (
+  //     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+  //   );
+  //   const data = await response.json()
+  //   setPostList(data);
+  //   setIsLoading(false);
+  // };
+
+  const fetchData = async (limit = 10) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+      );
+      const data = await response.json();
+      // console.log('API Response:', data);
+      setPostList(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+      setRefreshing(false); // Move setRefreshing(false) here will be called only after the fetching process completes
+    }
   };
 
+  const handleRefresh=()=>{
+    setRefreshing(true)
+    fetchData(20)
+    // setRefreshing(false)    :=setRefreshing(false) is called immediately after fetchData(20), before the fetching process completes. This causes the refreshing indicator to stop before the new data is actually fetched.
+  }
   useEffect( ()=>{
     fetchData();
-  });
+  },[]);
 
   if( isLoading ){
     return(
@@ -52,6 +75,8 @@ export default function App() {
           ListFooterComponent={
             <Text style={styles.footerText}>End of List</Text>
           }
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
          />
       </View>
     </SafeAreaView>
