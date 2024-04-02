7c05a2992446ac288cae206206987426f29eec98
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-02 19:49:26 +0530

    Error handling completed

--- networking/App.js
+++ networking/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, TextInput, Button} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { useState, useEffect } from 'react';
 
@@ -7,29 +7,31 @@ export default function App() {
   const [ postList, setPostList ] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [refreshing, setRefreshing] = useState(false)
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [error, setError] = useState("");
 
-  // const fetchData = async( limit = 10 ) => {
-  //   const response = await fetch (
-  //     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-  //   );
-  //   const data = await response.json()
-  //   setPostList(data);
-  //   setIsLoading(false);
-  // };
 
   const fetchData = async (limit = 10) => {
     try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-      );
+      );//https://jsonplaceholder.typicode.com/posts?_limit=${limit}
+      //localhost urls do not work from ANDROID emulator, you need workarounds by using pc's ip addr instead of local host
       const data = await response.json();
       // console.log('API Response:', data);
       setPostList(data);
+      setIsLoading(false)
+      setError("")
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error); //for logging error in console
+      setIsLoading(false)
+      setError("Failed to fetch postList") //for user
     } finally {
       setIsLoading(false);
       setRefreshing(false); // Move setRefreshing(false) here will be called only after the fetching process completes
+      
     }
   };
 
@@ -38,6 +40,38 @@ export default function App() {
     fetchData(20)
     // setRefreshing(false)    :=setRefreshing(false) is called immediately after fetchData(20), before the fetching process completes. This causes the refreshing indicator to stop before the new data is actually fetched.
   }
+
+  const addPost = async () => {
+    setIsPosting(true);
+
+    try{
+      //https://jsonplaceholder.typicode.com/posts
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts",
+    {
+      method : 'POST',
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      title: postTitle,
+      body: postBody
+    })
+    }
+    
+    )
+    const newPost = await response.json();
+    setPostList([newPost,...postList]);
+    setPostTitle("");
+    setPostBody("");
+    setIsPosting(false);
+    setError("")
+    }
+   catch(error){
+    console.error("Error in adding new post:", error)
+    setError("Failed to add new post")
+   } 
+  };
+
   useEffect( ()=>{
     fetchData();
   },[]);
@@ -52,6 +86,26 @@ export default function App() {
   }
   return (
     <SafeAreaView style={styles.container}>
+    {error ? (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+    ): (
+      <>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} 
+        placeholder='Post Title' 
+        value={postTitle} 
+        onChangeText={setPostTitle}/>
+        <TextInput style={styles.input} 
+        placeholder='Post Body' 
+        value={postBody} 
+        onChangeText={setPostBody}/>
+        <Button 
+        title={isPosting? "Adding...": "Add Post"} 
+        onPress={addPost}
+        disabled={isPosting}/>
+      </View>
       <View style={styles.listContainer}>
         <FlatList
         data={postList}
@@ -79,6 +133,8 @@ export default function App() {
           onRefresh={handleRefresh}
          />
       </View>
+      </>
+    )}
     </SafeAreaView>
   );
 }
@@ -122,5 +178,33 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent:"center",
     alignItems:"center"
+  },
+  inputContainer:{
+    backgroundColor:"white",
+    padding:16,
+    borderRadius:8,
+    borderWidth:1,
+    margin:16
+  },
+  input:{
+    height:40,
+    borderColor:"grey",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
+  },
+  errorContainer:{
+    backgroundColor: "#FFC0CB",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth:1,
+    margin: 16,
+    alignItems: "center"
+  },
+  errorText:{
+    color:"#D8000C",
+    fontSize: 16,
+    textAlign: "center"
   }
 });
