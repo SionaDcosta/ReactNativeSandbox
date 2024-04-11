e69e8c52a7c695de5218c16be6e4a1b49cee1127
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-11 12:34:10 +0530

    Used community conventions for reducers

--- randomColorWreducers/src/screens/SquareScreen.js
+++ randomColorWreducers/src/screens/SquareScreen.js
@@ -7,31 +7,39 @@ const COLOR_INCREMENT = 15;
 //action= how to change state obj
 const reducer = (state, action) =>{ 
   //state === { red: number, green: number, blue: number}
-  //action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+  //action(i.e howToChangeState) === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+  //Comm Con: action=== { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
+
+
   // When we want to change the property inside our state ob, we are going to rebuild that entire state obj from scratch and that new obj will have the changed value of r/g/b that we want
 
-  switch(action.colorToChange){
-    case 'red':
+  // switch(action.colorToChange){
+  switch(action.type){
+    case 'change_red':
       // if (state.red + action.amount > 255 || state.red + action.amount < 0){
       //   return state;
       // }
       // return { ...state, red: state.red + action.amount };
 
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+      return state.red + action.payload > 255 || state.red + action.payload < 0
       ? state
-      : { ...state, red: state.red + action.amount };
+      : { ...state, red: state.red + action.payload };
       // made a copy of the state var i.e return {red:0, green:0, blue:0}, 
       //then took red and redefined it( added amt to it) in the RHS of the existing obj, red will now be verwritten with the new value . This way Org state var will not be changed
     
-    case 'green':
-      return state.green + action.amount > 255 || state.green + action.amount < 0
+    case 'change_green':
+      return state.green + action.payload > 255 || state.green + action.payload < 0
       ? state
-      : { ...state, green: state.green + action.amount };
-      
-    case 'blue':
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
-      ? state
-      : { ...state, blue: state.blue + action.amount };
+      : { ...state, green: state.green + action.payload };
+
+      case 'change_blue':
+        return state.blue + action.payload > 255 || state.blue + action.payload < 0
+        ? state
+        : { ...state, blue: state.blue + action.payload };
+    // case 'blue':
+    //   return state.blue + action.amount > 255 || state.blue + action.amount < 0
+    //   ? state
+    //   : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
@@ -52,18 +60,20 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange:'red', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange:'red', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type:'change_red', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type:'change_red', payload: -1 * COLOR_INCREMENT})}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange:'blue', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange:'blue', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type:'change_blue', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type:'change_blue', payload: -1 * COLOR_INCREMENT})}
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange:'green', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({ colorToChange:'green', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({ type:'change_green', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({ type:'change_green', payload: -1 * COLOR_INCREMENT})}
+        // onIncrease={() => dispatch({ colorToChange:'green', amount: COLOR_INCREMENT})}
+        // onDecrease={() => dispatch({ colorToChange:'green', amount: -1 * COLOR_INCREMENT})}
         color="Green"
       />
       <View
