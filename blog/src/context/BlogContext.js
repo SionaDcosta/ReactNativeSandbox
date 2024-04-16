97e73839fac30c8bdae830252277c2faacaf0bc0
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-16 17:10:43 +0530

    Added useReducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider>
-        {children}
-    </BlogContext.Provider>
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []); //blogPosts can also be written as "state"
+
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
+
+
+
+
+// import React , {useState} from 'react';
+
+// const BlogContext = React.createContext();
+
+// export const BlogProvider = ({ children }) => {
+
+//     const [blogPosts, setBlogPosts] = useState([])
+    
+//     const addBlogPost = () => {
+//         setBlogPosts([ ...blogPosts, { title:  `Blog Post #${blogPosts.length+1}` } ]);
+//     };
+
+//     return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}> 
+//      {/*value={{ data: blogPosts, addBlogPost: addBlogPost(key=value Therefore , just write addBlogPost}}  */}
+//     {/* value={5} is the info that we want to share */}
+//         {children}
+//     </BlogContext.Provider>
+// };
+
+// export default BlogContext;
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,16 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import React, { useContext } from 'react'
+import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-  return (
+
+    // const value = useContext(BlogContext); // now this "value" var is going to be EXACTLY equal to the value that we passes from BlogContext
+    // const blogPosts = useContext(BlogContext);
+
+    //Now we no longer have an  array of obj/ array of data. So to destructure whatever we receive from blogContext
+    const {data, addBlogPost} = useContext(BlogContext);
+    
+    return (
     <View>
       <Text>IndexScreen</Text>
+      {/* <Text>{value}</Text> */}
+
+        <Button title='Add Post' onPress={addBlogPost}/>
+        {/* onPress={() => addBlogPost()}
+        here we are calling the arrow func, which calls another func . Therefore we can shorten this up by just passing reference to that func that we want to be called*/}
+      <FlatList
+        data={data}
+        keyExtractor={ blogPost => blogPost.title}
+        renderItem={({item} ) => {
+            return <Text>{item.title}</Text>
+
+        }}
+      />
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-    
+
 })
 export default IndexScreen
