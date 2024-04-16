64a8de85ec1280b8cb30c49026b9533e66d7ea2c
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-16 12:49:19 +0530

    context set up done.

--- blog/App.js
+++ blog/App.js
@@ -1,26 +1,28 @@
-import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Index"
-        screenOptions={{
-          headerTitle: "Blogs"
-        }}
-      >
-        <Stack.Screen
-          name="Index"
-          component={IndexScreen}
-        />
-        
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Index"
+          screenOptions={{
+            headerTitle: "Blogs"
+          }}
+        >
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
 
