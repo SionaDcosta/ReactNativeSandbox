b835f4a914ff9ae7948bd892bc947d0615a44e9a
SionaDcosta <siona.dcosta@zapcg.com>
2024-04-16 10:44:44 +0530

    initial navigation setup done.

---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
+export default IndexScreen
