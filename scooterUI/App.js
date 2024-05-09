3f06ed3b0ae2753fc2f4c5d0b6de157612791e9e
SionaDcosta <siona.dcosta@zapcg.com>
2024-05-09 16:26:57 +0530

    Initial commit

---
+++ scooterUI/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStackNavigator from './src/navigator/Navigation'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <HomeStackNavigator />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ scooterUI/src/navigator/Navigation.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from '../screens/Home'
+import Detail from '../screens/Detail'
+
+const Stack = createStackNavigator();
+
+
+const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Detail" component={Detail}/>
+    </Stack.Navigator>
+  )
+}
+
+export default HomeStackNavigator;
