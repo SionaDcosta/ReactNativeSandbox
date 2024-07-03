1fe8fd37cc40c9df01577a266fe629972bb81e20
SionaDcosta <siona.dcosta@zapcg.com>
2024-07-03 11:13:24 +0530

    initial

---
+++ movies/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import { View, Text, Platform, SafeAreaView, StatusBar, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+
+const ios = Platform.OS == 'ios'
+const HomeScreen = () => {
+    const [trending, setTrending] = useState([1,2,3])
+  return (
+    <View className='flex-1 bg-neutral-800'>
+        <SafeAreaView>
+            <StatusBar barStyle={'light-content'}/>
+            <View className='flex-row justify-between items-center mx-4'>
+                <Bars3CenterLeftIcon size={30} strokeWidth={2} color='white'/>
+                <Text className='text-white text-3xl font-bold'>Movies</Text>
+                <TouchableOpacity>
+                    <MagnifyingGlassIcon size={30} strokeWidth={2} color='white'/>
+                </TouchableOpacity>
+            </View>
+            
+        </SafeAreaView>
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom:10}}
+        >
+            <Text>1</Text>
+            <Text>1</Text>
+        </ScrollView>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
